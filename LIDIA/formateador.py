b56720531b2222b065fd7de6d963f1600594f6a3
MrHamburguesa <95600489+MrHamburguesa@users.noreply.github.com>
2022-08-05 11:27:37 -0400

    ACT 05/08/22

--- LIDIA/formateador.py
+++ LIDIA/formateador.py
@@ -7,16 +7,18 @@ with open('ASOCIACION_CODIGO_NOMBRE_REACTIVO.json', 'r') as f:
 
 def parseador(nombre_archivo):
     df = pd.read_excel(nombre_archivo)
-    df['Codigo crudo'] = df['Codigo crudo'].map(lambda x: str(x.replace('_x001D_', ' ')))
+    df['Codigo crudo'] = df['Codigo crudo'].map(lambda x: str(str(x).replace('_x001D_', ' ')))
     entradas = []
+    faltantes = []
     for entrada in df['Codigo crudo']:
         result = biip.parse(entrada, separator_chars = [' '])
         gtin = str(result.gtin.value)
         try:
             nombre = ASOCIACION_CODIGO_NOMBRE_REACTIVO[gtin]
         except KeyError:
-            print(f'El código {gtin} no está en la base de datos. Por favor, agregarlo al archivo ASOCIACION_CODIGO_NOMBRE_REACTIVO.json')
-            exit()
+            faltantes.append(gtin)
+            # print(f'El código {gtin} no está en la base de datos. Por favor, agregarlo al archivo ASOCIACION_CODIGO_NOMBRE_REACTIVO.json')
+            nombre = '?'
 
         fecha = str(result.gs1_message.get(data_title = 'EXPIRY').date)
         lote = str(result.gs1_message.get(ai = '10').value)
@@ -30,9 +32,15 @@ def parseador(nombre_archivo):
         entradas.append(linea)
 
     df = pd.DataFrame(entradas, columns = ['Nombre Reactivo', 'Gtin', 'Fecha Expiracion', 'Lote', 'Info adicional'])
+    faltantes = set(faltantes)
+
+    for gtin in faltantes:
+        print(f'El código {gtin} no está en la base de datos. Por favor, agregarlo al archivo ASOCIACION_CODIGO_NOMBRE_REACTIVO.json')
+
+    
     return df
 
 
 if __name__ == '__main__':
-    df = parseador('DATOS_CRUDOS_REACTIVOS.xlsx')
-    df.to_excel('DATOS_FORMATEADOS_REACTIVOS.xlsx', index = False)
+    df = parseador('REACTIVO RECIBIDOS 26-7-2022.xlsx')    
+    df.to_excel('INVENTARIO REACTIVOS 26-7-2022.xlsx', index = False)
