90d4b72e14cd75eceb4a254bfa6b84425bb53bec
MrHamburguesa <95600489+MrHamburguesa@users.noreply.github.com>
2022-08-05 11:49:42 -0400

    Se hace bien el inventario

--- LIDIA/formateador.py
+++ LIDIA/formateador.py
@@ -5,19 +5,34 @@ import json
 with open('ASOCIACION_CODIGO_NOMBRE_REACTIVO.json', 'r') as f:
     ASOCIACION_CODIGO_NOMBRE_REACTIVO = json.load(f)
 
-def parseador(nombre_archivo):
-    df = pd.read_excel(nombre_archivo)
+
+
+def hacer_inventario(nombre_archivo):
+    tabla_entrada = hacer_tabla(nombre_archivo, 0)
+    tabla_salida = hacer_tabla(nombre_archivo, 1)
+    tabla_actual = tabla_entrada[~tabla_entrada['Nombre Reactivo'].isin(tabla_salida['Nombre Reactivo'])]
+    resumen_inventario = pd.DataFrame(tabla_actual['Nombre Reactivo'].value_counts())
+
+    with pd.ExcelWriter('INVENTARIO REACTIVOS 26-7-2022.xlsx') as writer:
+        tabla_entrada.to_excel(writer, index = False, sheet_name = 'ENTRADA')
+        tabla_salida.to_excel(writer, index = False, sheet_name = 'SALIDA')
+        resumen_inventario.to_excel(writer, sheet_name = 'ACTUAL')
+                                                        
+
+def hacer_tabla(nombre_archivo, hoja):
+    df = pd.read_excel(nombre_archivo, sheet_name = hoja)
     df['Codigo crudo'] = df['Codigo crudo'].map(lambda x: str(str(x).replace('_x001D_', ' ')))
     entradas = []
     faltantes = []
-    for entrada in df['Codigo crudo']:
+
+    for i in range(df.shape[0]):
+        entrada = df.iloc[i, 0]
         result = biip.parse(entrada, separator_chars = [' '])
         gtin = str(result.gtin.value)
         try:
             nombre = ASOCIACION_CODIGO_NOMBRE_REACTIVO[gtin]
         except KeyError:
             faltantes.append(gtin)
-            # print(f'El código {gtin} no está en la base de datos. Por favor, agregarlo al archivo ASOCIACION_CODIGO_NOMBRE_REACTIVO.json')
             nombre = '?'
 
         fecha = str(result.gs1_message.get(data_title = 'EXPIRY').date)
@@ -27,11 +42,13 @@ def parseador(nombre_archivo):
             info_adicional = str(result.gs1_message.get(ai = '240').value)
         except AttributeError:
             info_adicional = None
+
+        fecha_pistoleo = df.iloc[i, 1]
         
-        linea = [nombre, gtin, fecha, lote, info_adicional]
+        linea = [nombre, gtin, fecha, lote, info_adicional, fecha_pistoleo]
         entradas.append(linea)
 
-    df = pd.DataFrame(entradas, columns = ['Nombre Reactivo', 'Gtin', 'Fecha Expiracion', 'Lote', 'Info adicional'])
+    df = pd.DataFrame(entradas, columns = ['Nombre Reactivo', 'Gtin', 'Fecha Expiracion', 'Lote', 'Info adicional', 'Fecha pistoleo'])
     faltantes = set(faltantes)
 
     for gtin in faltantes:
@@ -42,5 +59,4 @@ def parseador(nombre_archivo):
 
 
 if __name__ == '__main__':
-    df = parseador('REACTIVO RECIBIDOS 26-7-2022.xlsx')    
-    df.to_excel('INVENTARIO REACTIVOS 26-7-2022.xlsx', index = False)
+    hacer_inventario('REACTIVO RECIBIDOS 26-7-2022.xlsx')
