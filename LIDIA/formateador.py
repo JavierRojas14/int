de48e8d88bc7e7fbf506875455178fa97db3b8dc
MrHamburguesa <95600489+MrHamburguesa@users.noreply.github.com>
2022-08-18 15:42:54 -0400

    Add files via upload

--- LIDIA/formateador.py
+++ LIDIA/formateador.py
@@ -6,26 +6,42 @@ from datetime import datetime
 with open('ASOCIACION_CODIGO_NOMBRE_REACTIVO.json', 'r') as f:
     ASOCIACION_CODIGO_NOMBRE_REACTIVO = json.load(f)
 
+def agrupar_por_lote_tablas(tabla):
+    desglose = pd.DataFrame(tabla.groupby(['Nombre Reactivo', 'Lote']).count().iloc[:, 0])
+    desglose.columns = ['Conteo']
+    return desglose
+    
+
 def hacer_inventario(nombre_archivo):
     tabla_entrada = hacer_tabla(nombre_archivo, 0)
-    tabla_salida = hacer_tabla(nombre_archivo, 1)
     conteo_entrada = tabla_entrada.index.value_counts()
+    desglose_entrada = agrupar_por_lote_tablas(tabla_entrada)
+    
+    tabla_salida = hacer_tabla(nombre_archivo, 1)
     conteo_salida = tabla_salida.index.value_counts()
+    desglose_salida = agrupar_por_lote_tablas(tabla_salida)
 
-    desglose_entrada = pd.DataFrame(tabla_entrada.groupby(['Nombre Reactivo', 'Lote']).count().iloc[:, 0])
-    desglose_entrada.columns = ['Conteo']
-    desglose_salida = pd.DataFrame(tabla_salida.groupby(['Nombre Reactivo', 'Lote']).count().iloc[:, 0])
-    desglose_salida.columns = ['Conteo']
-
-    inventario = pd.DataFrame(conteo_entrada.subtract(conteo_salida, fill_value = 0).astype(int))
-    inventario_desglosado = pd.DataFrame(desglose_entrada.subtract(desglose_salida, fill_value = 0).astype(int))
+    conteo_inventario = pd.DataFrame(conteo_entrada.subtract(conteo_salida, fill_value = 0).astype(int))
+    desglose_inventario = pd.DataFrame(desglose_entrada.subtract(desglose_salida, fill_value = 0).astype(int))
 
     with pd.ExcelWriter('INVENTARIO REACTIVOS 26-7-2022.xlsx') as writer:
         fecha_actual = datetime.now().strftime("%d-%m-%Y %H_%M_%S")
+
         tabla_entrada.to_excel(writer, sheet_name = 'ENTRADA')
+        conteo_entrada = pd.DataFrame(conteo_entrada)
+        conteo_entrada.columns = ['Conteo']
+        conteo_entrada.to_excel(writer, sheet_name = 'ENTRADA', startcol = 7)
+        desglose_entrada.to_excel(writer, sheet_name = 'ENTRADA', startcol = 10)
+        
         tabla_salida.to_excel(writer, sheet_name = 'SALIDA')
-        inventario.to_excel(writer, sheet_name = f'INV {fecha_actual}')
-        inventario_desglosado.to_excel(writer, sheet_name = f'INV DESG {fecha_actual}')
+        conteo_salida = pd.DataFrame(conteo_salida)
+        conteo_salida.columns = ['Conteo']
+        conteo_salida.to_excel(writer, sheet_name = 'SALIDA', startcol = 7)
+        desglose_salida.to_excel(writer, sheet_name = 'SALIDA', startcol = 10)
+        
+        conteo_inventario.to_excel(writer, sheet_name = f'INV {fecha_actual}')
+        conteo_inventario.columns = ['Conteo']
+        desglose_inventario.to_excel(writer, sheet_name = f'INV {fecha_actual}', startcol = 5)
     
     return tabla_entrada, tabla_salida
                                                         
