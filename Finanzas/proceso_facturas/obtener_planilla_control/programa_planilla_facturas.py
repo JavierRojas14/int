bfa5318a950cfc126e003e86562978adc9907576
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-16 14:53:24 -0300

    Add filter of files to read based on condition

--- Finanzas/proceso_facturas/obtener_planilla_control/programa_planilla_facturas.py
+++ Finanzas/proceso_facturas/obtener_planilla_control/programa_planilla_facturas.py
@@ -13,12 +13,12 @@ from pandas.core.arrays import base
 pd.options.mode.chained_assignment = None  # default='warn'
 
 
-
 class GeneradorPlanillaFinanzas:
     '''
     Esta es la clase madre que permite obtener la planilla de control de facturas.
     Consta de 6 funciones principales.
     '''
+
     def __init__(self):
         pass
 
@@ -45,6 +45,29 @@ class GeneradorPlanillaFinanzas:
 
         print('\nListo! No hubo ningún problema')
 
+    def obtener_archivos_a_leer(self):
+        todos_los_archivos = {}
+        for carpeta_base_de_datos in os.listdir('crudos'):
+            todos_los_archivos[carpeta_base_de_datos] = []
+            for archivo in os.listdir(os.path.join('crudos', carpeta_base_de_datos)):
+                ruta_archivo = os.path.join('crudos', carpeta_base_de_datos, archivo)
+                todos_los_archivos[carpeta_base_de_datos].append(ruta_archivo)
+
+        hoy = datetime.date.today()
+        anio_actual = str(hoy.year)
+
+        leer = input('¿Quieres leer los archivos de este año o todos los años? \n'
+                     '1) Este año \n'
+                     '2) Todos los años \n'
+                     '> ')
+
+        if leer == '1':
+            for base_de_datos, lista_archivos in todos_los_archivos.items():
+                todos_los_archivos[base_de_datos] = [
+                    archivo for archivo in lista_archivos if anio_actual in archivo]
+
+        return todos_los_archivos
+
     def leer_y_limpiar_dfs(self):
         '''
         Esta función permite leer las bases de datos del SII, ACEPTA, SIGFE, TURBO, SCI y
@@ -64,30 +87,30 @@ class GeneradorPlanillaFinanzas:
             if base_de_datos == 'ACEPTA':
                 dfs = list(map(pd.read_excel, ruta_dfs))
                 df_sumada = pd.concat(dfs)
-                df_sumada = df_sumada.rename(columns = {'emisor': 'RUT Emisor', 'folio': 'Folio'})
+                df_sumada = df_sumada.rename(columns={'emisor': 'RUT Emisor', 'folio': 'Folio'})
 
             elif base_de_datos == 'SCI':
-                dfs = list(map(lambda x: pd.read_csv(x, delimiter = ','), ruta_dfs))
+                dfs = list(map(lambda x: pd.read_csv(x, delimiter=','), ruta_dfs))
                 df_sumada = pd.concat(dfs)
-                df_sumada = df_sumada.rename(columns = {'Rut Proveedor': 'RUT Emisor',
-                                                        'Numero Documento': 'Folio'})
+                df_sumada = df_sumada.rename(columns={'Rut Proveedor': 'RUT Emisor',
+                                                      'Numero Documento': 'Folio'})
 
                 df_sumada['Folio'] = df_sumada['Folio'].astype(str) \
-                                                    .str.replace('.0', '', regex = False)
+                    .str.replace('.0', '', regex=False)
 
             elif base_de_datos == 'SIGFE':
-                dfs = list(map(lambda x: pd.read_csv(x, delimiter = ',', header = 10), ruta_dfs))
+                dfs = list(map(lambda x: pd.read_csv(x, delimiter=',', header=10), ruta_dfs))
                 df_sumada = pd.concat(dfs)
-                df_sumada = df_sumada.dropna(subset = ['Folio'])
+                df_sumada = df_sumada.dropna(subset=['Folio'])
                 df_sumada = df_sumada.query('`Cuenta Contable` != "Cuenta Contable"')
 
                 df_sumada['RUT Emisor'] = df_sumada['Principal'].str.split(' ').str[0]
 
-                df_sumada = df_sumada.rename(columns = {'Folio': 'Folio_interno',
-                                                        'Número ': 'Folio'})
+                df_sumada = df_sumada.rename(columns={'Folio': 'Folio_interno',
+                                                      'Número ': 'Folio'})
                 df_sumada = df_sumada.reset_index()
 
-                df_sumada['Fecha'] = pd.to_datetime(df_sumada['Fecha'], dayfirst = True)
+                df_sumada['Fecha'] = pd.to_datetime(df_sumada['Fecha'], dayfirst=True)
                 df_sumada['Folio_interno'] = df_sumada['Folio_interno'].astype('Int32')
 
                 mask_debe = (df_sumada['Debe'] != "0")
@@ -100,50 +123,50 @@ class GeneradorPlanillaFinanzas:
                 df_sumada['Fecha DEVENGO'] = df_sumada['Fecha'][mask_haber]
 
             elif base_de_datos == 'SII':
-                dfs = list(map(lambda x: pd.read_csv(x, delimiter = ';', index_col = False),
-                            ruta_dfs))
+                dfs = list(map(lambda x: pd.read_csv(x, delimiter=';', index_col=False),
+                               ruta_dfs))
 
-                dfs = list(map(lambda x: x.drop(columns = ['Tabacos Puros', 'Tabacos Cigarrillos',
-                                                        'Tabacos Elaborados']) \
-                                        if len(x.columns) == 27 \
-                                        else x, dfs))
+                dfs = list(map(lambda x: x.drop(columns=['Tabacos Puros', 'Tabacos Cigarrillos',
+                                                         'Tabacos Elaborados'])
+                               if len(x.columns) == 27
+                               else x, dfs))
 
-                df_sumada = pd.concat(dfs).rename(columns = {'RUT Proveedor': 'RUT Emisor'})
+                df_sumada = pd.concat(dfs).rename(columns={'RUT Proveedor': 'RUT Emisor'})
                 mask_negativas = (df_sumada['Tipo Doc'] == 61) | (df_sumada['Tipo Doc'] == 56)
                 columnas_negativas = ['Monto Exento', 'Monto Neto', 'Monto IVA Recuperable',
-                                    'Monto Total']
+                                      'Monto Total']
 
                 df_sumada.loc[mask_negativas, columnas_negativas] = df_sumada.loc[mask_negativas,
-                                                                    columnas_negativas] * -1
+                                                                                  columnas_negativas] * -1
 
             elif base_de_datos == 'TURBO':
-                dfs = list(map(lambda x: pd.read_excel(x, header = 3), ruta_dfs))
+                dfs = list(map(lambda x: pd.read_excel(x, header=3), ruta_dfs))
                 df_sumada = pd.concat(dfs)
-                df_sumada = df_sumada.rename(columns = {'Rut': 'RUT Emisor',
-                                                        'Folio': 'Folio_interno',
-                                                        'NºDoc.': 'Folio'})
+                df_sumada = df_sumada.rename(columns={'Rut': 'RUT Emisor',
+                                                      'Folio': 'Folio_interno',
+                                                      'NºDoc.': 'Folio'})
 
                 df_sumada['Folio'] = df_sumada['Folio'].astype(str) \
-                                                    .str.replace('.0', '', regex = False)
+                    .str.replace('.0', '', regex=False)
 
             elif base_de_datos == 'OBSERVACIONES':
                 dfs = list(map(pd.read_excel, ruta_dfs))
                 df_sumada = pd.concat(dfs)
                 df_sumada = df_sumada[['RUT Emisor SII', 'Folio SII', 'OBSERVACION OBSERVACIONES']]
-                df_sumada = df_sumada.rename(columns = {'RUT Emisor SII': 'RUT Emisor',
-                                                        'Folio SII': 'Folio',
-                                                        'OBSERVACION OBSERVACIONES': 'OBSERVACION'})
+                df_sumada = df_sumada.rename(columns={'RUT Emisor SII': 'RUT Emisor',
+                                                      'Folio SII': 'Folio',
+                                                      'OBSERVACION OBSERVACIONES': 'OBSERVACION'})
 
-            df_sumada['RUT Emisor'] = df_sumada['RUT Emisor'].str.replace('.', '', regex = False) \
-                                                    .str.upper() \
-                                                    .str.strip()
+            df_sumada['RUT Emisor'] = df_sumada['RUT Emisor'].str.replace('.', '', regex=False) \
+                .str.upper() \
+                .str.strip()
 
             df_sumada.columns = df_sumada.columns + f' {base_de_datos}'
 
             df_sumada['llave_id'] = df_sumada[f'RUT Emisor {base_de_datos}'].astype(str) + \
-                                    df_sumada[f'Folio {base_de_datos}'].astype(str)
+                df_sumada[f'Folio {base_de_datos}'].astype(str)
 
-            df_sumada.set_index('llave_id', drop = True, inplace = True)
+            df_sumada.set_index('llave_id', drop=True, inplace=True)
 
             diccionario_dfs[base_de_datos] = df_sumada
 
@@ -153,7 +176,7 @@ class GeneradorPlanillaFinanzas:
         fecha_pago = df_sigfe.groupby('llave_id')['Fecha PAGO SIGFE'].min()
         folio_pago = df_sigfe.groupby('llave_id')['Folio_interno PAGO SIGFE'].min()
         juntos = pd.concat([fecha_devengo_mas_antigua, folio_devengo, fecha_pago, folio_pago],
-                            axis = 1)
+                           axis=1)
 
         diccionario_dfs['SIGFE'] = juntos
 
@@ -170,12 +193,11 @@ class GeneradorPlanillaFinanzas:
         lista_dfs_secuenciales = list(diccionario_dfs_limpias.values())
         df_izquierda = lista_dfs_secuenciales.pop(4)
 
-
         for df_derecha in lista_dfs_secuenciales:
-            df_izquierda = pd.merge(df_izquierda, df_derecha, how = 'left', left_index = True,
-                                                                            right_index = True)
+            df_izquierda = pd.merge(df_izquierda, df_derecha, how='left', left_index=True,
+                                    right_index=True)
 
-        df_izquierda = df_izquierda[~df_izquierda.index.duplicated(keep = 'first')]
+        df_izquierda = df_izquierda[~df_izquierda.index.duplicated(keep='first')]
 
         return df_izquierda
 
@@ -189,22 +211,22 @@ class GeneradorPlanillaFinanzas:
         mask_no_devengadas = pd.isna(df_izquierda['Fecha DEVENGO SIGFE'])
 
         df_izquierda['Fecha Docto SII'] = pd.to_datetime(df_izquierda['Fecha Docto SII'],
-                                                         dayfirst = True)
+                                                         dayfirst=True)
 
         df_izquierda['Fecha Recepcion SII'] = pd.to_datetime(df_izquierda['Fecha Recepcion SII'],
-                                                             dayfirst = True)
+                                                             dayfirst=True)
 
         df_izquierda['Fecha Reclamo SII'] = pd.to_datetime(df_izquierda['Fecha Reclamo SII'],
-                                            dayfirst = True)
+                                                           dayfirst=True)
 
         diferencia = (pd.to_datetime('today')
                       - df_izquierda[mask_no_devengadas]['Fecha Recepcion SII']) \
-                      + pd.Timedelta(days = 1)
+            + pd.Timedelta(days=1)
 
         df_izquierda['tiempo_diferencia SII'] = diferencia
 
         esta_al_dia = df_izquierda[mask_no_devengadas]['tiempo_diferencia SII'] \
-                      <= datetime.timedelta(8)
+            <= datetime.timedelta(8)
 
         df_izquierda['esta_al_dia'] = esta_al_dia
 
@@ -215,7 +237,7 @@ class GeneradorPlanillaFinanzas:
         Esta función permite obtener las referencias que tienen las Notas de Crédito dentro la
         base de datos ACEPTA
         '''
-        diccionario_json = json.loads(string_json, strict = False)
+        diccionario_json = json.loads(string_json, strict=False)
         for documento_referencia in diccionario_json:
             if documento_referencia['Tipo'] == '33':
                 return documento_referencia['Folio']
@@ -250,7 +272,7 @@ class GeneradorPlanillaFinanzas:
                 mask_boletas_referenciadas = df_izquierda.index == referencia
                 df_izquierda.loc[mask_boletas_referenciadas, 'REFERENCIAS'] = nota_c
 
-        df_izquierda = df_izquierda.drop(columns = 'LLAVES REFERENCIAS PARA NC')
+        df_izquierda = df_izquierda.drop(columns='LLAVES REFERENCIAS PARA NC')
 
         return df_izquierda
 
@@ -259,13 +281,15 @@ class GeneradorPlanillaFinanzas:
         Esta función permite agregar el saldo disponible de las ordenes de compra a cada
         factura que esté asociada.
         '''
-        oc_sigfe = pd.read_excel('crudos\\analisis_posterior_cruce\\SIGFE REPORTS\\SA_ListadoDisponibilidadCompromiso.xls', header = 5)
+        oc_sigfe = pd.read_excel(
+            'crudos\\analisis_posterior_cruce\\SIGFE REPORTS\\SA_ListadoDisponibilidadCompromiso.xls',
+            header=5)
         # oc_pendientes = oc_sigfe.query('`Monto Disponible` > 0')
         # mask_subtitulo_22 = oc_pendientes['Concepto Presupuesto'].str[:2] == '22'
         # oc_pendientes_subt_22 = oc_pendientes[mask_subtitulo_22]
 
         for orden_compra in oc_sigfe['Número Documento'].unique():
-            if not(orden_compra in ['2022', '2']):
+            if not (orden_compra in ['2022', '2']):
 
                 mask_oc_sigfe = (oc_sigfe['Número Documento'] == orden_compra)
                 datos_oc = oc_sigfe[mask_oc_sigfe]
@@ -281,7 +305,6 @@ class GeneradorPlanillaFinanzas:
 
         return df_junta
 
-
     def obtener_columnas_necesarias(self, df_izquierda):
         '''
         Esta función selecciona sólo las columnas necesarias en el fomrato final para
@@ -294,22 +317,23 @@ class GeneradorPlanillaFinanzas:
         - Utiliza 4 columnas de TURBO
         - Utiliza 4 columnas calculadas internamente.
         '''
-        columnas_a_ocupar = ['Tipo Doc SII', 'RUT Emisor SII', 'Razon Social SII', 'Folio SII',
-        'Fecha Docto SII','Fecha Recepcion SII', 'Fecha Reclamo SII', 'Monto Exento SII',
-        'Monto Neto SII', 'Monto IVA Recuperable SII', 'Monto Total SII',
-        'publicacion ACEPTA', 'estado_acepta ACEPTA', 'estado_sii ACEPTA', 'estado_nar ACEPTA',
-        'estado_devengo ACEPTA', 'folio_oc ACEPTA', 'folio_rc ACEPTA', 'fecha_ingreso_rc ACEPTA',
-        'folio_sigfe ACEPTA', 'tarea_actual ACEPTA', 'estado_cesion ACEPTA',
-        'Fecha DEVENGO SIGFE', 'Folio_interno DEVENGO SIGFE', 'Fecha PAGO SIGFE',
-        'Folio_interno PAGO SIGFE',
-        'Fecha Recepción SCI', 'Registrador SCI', 'Articulo SCI', 'N° Acta SCI',
-        'Ubic. TURBO', 'NºPresu TURBO', 'Folio_interno TURBO', 'NºPago TURBO',
-        'tiempo_diferencia SII', 'esta_al_dia', 'REFERENCIAS', 'OBSERVACION OBSERVACIONES',
-        'NUMERO_COMPROMISO_OC', 'MONTO_DISPONIBLE_OC']
+        columnas_a_ocupar = [
+            'Tipo Doc SII', 'RUT Emisor SII', 'Razon Social SII', 'Folio SII', 'Fecha Docto SII',
+            'Fecha Recepcion SII', 'Fecha Reclamo SII', 'Monto Exento SII', 'Monto Neto SII',
+            'Monto IVA Recuperable SII', 'Monto Total SII', 'publicacion ACEPTA',
+            'estado_acepta ACEPTA', 'estado_sii ACEPTA', 'estado_nar ACEPTA',
+            'estado_devengo ACEPTA', 'folio_oc ACEPTA', 'folio_rc ACEPTA',
+            'fecha_ingreso_rc ACEPTA', 'folio_sigfe ACEPTA', 'tarea_actual ACEPTA',
+            'estado_cesion ACEPTA', 'Fecha DEVENGO SIGFE', 'Folio_interno DEVENGO SIGFE',
+            'Fecha PAGO SIGFE', 'Folio_interno PAGO SIGFE', 'Fecha Recepción SCI',
+            'Registrador SCI', 'Articulo SCI', 'N° Acta SCI', 'Ubic. TURBO', 'NºPresu TURBO',
+            'Folio_interno TURBO', 'NºPago TURBO', 'tiempo_diferencia SII', 'esta_al_dia',
+            'REFERENCIAS', 'OBSERVACION OBSERVACIONES', 'NUMERO_COMPROMISO_OC',
+            'MONTO_DISPONIBLE_OC']
 
         df_util = df_izquierda[columnas_a_ocupar]
         df_util['Tipo Doc SII'] = df_util['Tipo Doc SII'].astype('category')
-        df_util = df_util.sort_values(by = ['Fecha Docto SII'])
+        df_util = df_util.sort_values(by=['Fecha Docto SII'])
 
         return df_util
 
@@ -319,11 +343,12 @@ class GeneradorPlanillaFinanzas:
         - El nombre del archivo es PLANILLA DE CONTROL AL  (fecha actual)
         - Se formatea automáticamente la fecha al escribirse a formato excel.
         '''
-        fecha_actual = str(pd.to_datetime('today')).split(' ', maxsplit = 1)[0]
+        fecha_actual = str(pd.to_datetime('today')).split(' ', maxsplit=1)[0]
         nombre_archivo = f'PLANILLA DE CONTROL AL {fecha_actual}.xlsx'
 
-        with pd.ExcelWriter(nombre_archivo, datetime_format = 'DD-MM-YYYY') as writer:
+        with pd.ExcelWriter(nombre_archivo, datetime_format='DD-MM-YYYY') as writer:
             df_columnas_utiles.to_excel(writer)
+
 
 start_time = time.time()
 programa = GeneradorPlanillaFinanzas()
