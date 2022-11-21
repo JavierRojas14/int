7ccda5c19f920451a9c1c8011b1fe4fa9145fa3e
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-21 14:15:31 -0300

    Add method to read producciones file

---
+++ Finanzas/planilla_centro_de_costos_sigcom/OCTUBRE_2022/4_suministros/cargar_archivo_producciones.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import sys 
+
+
+def cargar_archivo():
+    mes_a_analizar = sys.argv[1]
+
+    nombre_archivo = [nombre for nombre in os.listdir('input') if 'Producción' in nombre][0]
+    nombre_archivo = os.path.join('input', nombre_archivo)
+    producciones = pd.read_excel(nombre_archivo)
+
+    producciones = producciones.loc[:, 'SERVICIOS FINALES':'TOTAL AÑO']
+    producciones.columns = [
+        'SERVICIOS FINALES', 'ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO',
+        'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE', 'TOTAL AÑO']
+
+    producciones['SERVICIOS FINALES'] = producciones['SERVICIOS FINALES'].fillna('PLACEHOLDER')
+
+    df_hospitalizaciones = producciones.loc[0:2, mes_a_analizar]
+    df_producciones = producciones.loc[3:, mes_a_analizar]
+
+    return df_hospitalizaciones, df_producciones
+
+
+if __name__ == '__main__':
+    hospitalizaciones_mes, producciones_mes = cargar_archivo()
+    print(hospitalizaciones_mes)
+    print(producciones_mes)
