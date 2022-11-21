d08d7622662f169da5c2df2077b18f5b59b12d27
MrHamburguesa <javierrojas14.jr@gmail.com>
2022-11-21 14:34:54 -0300

    Change columns that are included in last dfs

--- Finanzas/planilla_centro_de_costos_sigcom/OCTUBRE_2022/4_suministros/cargar_archivo_producciones.py
+++ Finanzas/planilla_centro_de_costos_sigcom/OCTUBRE_2022/4_suministros/cargar_archivo_producciones.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sys 
 
 
-def cargar_archivo():
+def cargar_archivo_produccion():
     mes_a_analizar = sys.argv[1]
 
     nombre_archivo = [nombre for nombre in os.listdir('input') if 'Producción' in nombre][0]
@@ -17,13 +17,13 @@ def cargar_archivo():
 
     producciones['SERVICIOS FINALES'] = producciones['SERVICIOS FINALES'].fillna('PLACEHOLDER')
 
-    df_hospitalizaciones = producciones.loc[0:2, mes_a_analizar]
-    df_producciones = producciones.loc[3:, mes_a_analizar]
+    df_hospitalizaciones = producciones.loc[0:2, ['SERVICIOS FINALES', mes_a_analizar]]
+    df_producciones = producciones.loc[3:, ['SERVICIOS FINALES', mes_a_analizar]]
 
     return df_hospitalizaciones, df_producciones
 
 
 if __name__ == '__main__':
-    hospitalizaciones_mes, producciones_mes = cargar_archivo()
+    hospitalizaciones_mes, producciones_mes = cargar_archivo_produccion()
     print(hospitalizaciones_mes)
     print(producciones_mes)
